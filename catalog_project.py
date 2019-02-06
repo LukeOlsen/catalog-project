1728711a70e1d95519bdd1db495a49a5be9cc2dc
Luke Olsen <lukeaolsen@gmail.com>
2019-02-06 17:56:20 -0500

    More routing work.

--- catalog_project.py
+++ catalog_project.py
@@ -20,11 +20,33 @@ def renderItemGroups():
     item_groups = session.query(ClothingGroup).all()
     return render_template('groups.html', item_groups = item_groups)
 
-@app.route('/clothing/<int:clothing_group_id>')
+@app.route('/clothing/<int:clothing_group_id>/')
 def renderItemGroupList(clothing_group_id):
     item_group = session.query(ClothingGroup).filter_by(id = clothing_group_id).one()
     items = session.query(ClothingItem).filter_by(item_group_id = item_group.id)
     return render_template('itemlist.html', items = items)
+
+@app.route('/clothing/newItemGroup/', methods=['GET', 'POST'])
+def addNewItemGroup():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('additemgroup.html')
+
+@app.route('/clothing/<int:clothing_group_id>/<int:item_id>/', method=['GET', 'POST'])
+def addNewItem(clothing_group_id, item_id):
+    if request.method == 'POST':
+        pass
+    else: 
+        return render_template('additem.html')
+
+@app.route('/clothing/<int:clothing_group_id>/<int:item_id>/edit', method=['GET', 'POST'])
+def editItem(clothing_group_id, item_id):
+    if request.method == 'POST':
+        pass
+    else: 
+        return render_template('edititem.html')
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
