9ab8a7a5b194baaf3ef71aa06d64fc00c0b9f688
Luke Olsen <lukeaolsen@gmail.com>
2019-02-06 20:47:26 -0500

    Finished creating initial routes for CRUD procedures.

--- catalog_project.py
+++ catalog_project.py
@@ -26,6 +26,10 @@ def renderItemGroupList(clothing_group_id):
     items = session.query(ClothingItem).filter_by(item_group_id = item_group.id)
     return render_template('itemlist.html', items = items)
 
+@app.route('clothing/<int:clothing_group_id>/<int:item_id>/')
+def renderItem(clothing_group_id, item_id):
+    return render_template('item.html', clothing_group_id = clothing_group_id, item_id = item_id)
+
 @app.route('/clothing/newItemGroup/', methods=['GET', 'POST'])
 def addNewItemGroup():
     if request.method == 'POST':
@@ -33,19 +37,40 @@ def addNewItemGroup():
     else:
         return render_template('additemgroup.html')
 
-@app.route('/clothing/<int:clothing_group_id>/<int:item_id>/', method=['GET', 'POST'])
-def addNewItem(clothing_group_id, item_id):
+@app.route('/clothing/<int:clothing_group_id>/newItem', methods=['GET', 'POST'])
+def addNewItem(clothing_group_id):
     if request.method == 'POST':
         pass
     else: 
         return render_template('additem.html')
 
-@app.route('/clothing/<int:clothing_group_id>/<int:item_id>/edit', method=['GET', 'POST'])
+@app.route('/clothing/<int:clothing_group_id>/edit', methods=['GET', 'POST'])
+def editItemGroup(clothing_group_id):
+    if request.method == 'POST':
+        pass
+    else: 
+        return render_template('editgroup.html')
+
+@app.route('/clothing/<int:clothing_group_id>/<int:item_id>/edit', methods=['GET', 'POST'])
 def editItem(clothing_group_id, item_id):
     if request.method == 'POST':
         pass
     else: 
         return render_template('edititem.html')
+
+@app.route('/clothing/<int:clothing_group_id>/delete', methods=['GET', 'POST'])
+def deleteItemGroup(clothing_group_id):
+    if request.method == 'POST':
+        pass
+    else: 
+        return render_template('deleteitemgroup.html')
+
+@app.route('/clothing/<int:clothing_group_id>/<int:item_id>/delete', methods=['GET', 'POST'])
+def deleteItem(clothing_group_id, item_id):
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('deleteitem.html')
 
 
 if __name__ == '__main__':
